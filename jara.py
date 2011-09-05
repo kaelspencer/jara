54e4443050495977f3e1f1b7a09a349c9d8a28f9
Kael Spencer <kael@euler>
2011-09-04 17:18:18 -0700

    Removed duplicate games. Removed FCS schools. Fixed home/away issue.

--- jara.py
+++ jara.py
@@ -6,21 +6,43 @@ class School:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.games = []
+        self.games = set([])
         
     def __str__(self):
-        return self.name + ' (' + self.id + ')'
+        return self.name
         
     def AddGame(self, g):
-        self.games.append(g)
+        self.games.add(g)
+    
+    def WinCount(self):
+        count = 0;
+        
+        for g in self.games:
+            if g.winner == self:
+                count += 1
+        
+        return count
+    
+    def LossCount(self):
+        count = 0;
+        
+        for g in self.games:
+            if g.loser == self:
+                count += 1
+        
+        return count
+            
+    def __eq__(self, other):
+        return self.id == other.id
 
 class Game:
-    def __init__(self, home, away, hscore, ascore, date):
+    def __init__(self, home, away, hscore, ascore, date, neutral):
         self.home = home
         self.away = away
-        self.hscore = hscore
-        self.ascore = ascore
+        self.hscore = int(hscore)
+        self.ascore = int(ascore)
         self.date = date
+        self.neutral = neutral
         
         if self.hscore > self.ascore:
             self.winner = self.home
@@ -34,20 +56,42 @@ class Game:
             self.lscore = self.hscore
     
     def __str__(self):
-        return self.winner.name + ' over ' + self.loser.name + ': ' + self.wscore + ' - ' + self.lscore
+        output = self.winner.name + ' over ' + self.loser.name + ': ' + str(self.wscore) + ' - ' + str(self.lscore)
+        
+        if self.neutral:
+            output += ' (Neutral field)'
+        else:
+            output += ' (At ' + self.home.name + ')'
+        
+        return output
+    
+    def __hash__(self):
+        return hash(str(self))
+    
+    def __cmp__(self, other):
+        if str(self) == str(other):
+            return 0
+        else:
+            return 1
     
 def AddGame(row):
-    if row['InstitutionID'] in schools:
-        h = schools[row['InstitutionID']]
+    neutral = False
+
+    if row['InstitutionID'] in allSchools:
+        h = allSchools[row['InstitutionID']]
     else:
         h = School(row['InstitutionID'], row['Institution'])
-        schools[h.id] = h
+        fbsSchools[h.id] = h
+        allSchools[h.id] = h
 
-    if row['Opponent ID'] in schools:
-        a = schools[row['Opponent ID']]
+    if row['Opponent ID'] in allSchools:
+        a = allSchools[row['Opponent ID']]
     else:
         a = School(row['Opponent ID'], row['Opponent Name'])
-        schools[a.id] = a
+        allSchools[h.id] = h
+        
+    if row['Location'] == 'Neutral Site':
+        neutral = True
 
     if row['Location'] == 'Away':
         temp = h
@@ -59,19 +103,20 @@ def AddGame(row):
         hscore = row['Score For']
         ascore = row['Score Against']
     
-    g = Game(h, a, hscore, ascore, row['Game Date'])
+    g = Game(h, a, hscore, ascore, row['Game Date'], neutral)
     h.AddGame(g)
     a.AddGame(g)
 
-schools = dict()
+fbsSchools = dict()
+allSchools = dict()
 
 reader = csv.DictReader(open('fbs.2010.final.csv', 'rb'), delimiter=',', quotechar='"')
 
 for row in reader:
     AddGame(row)
 
-for id, s in schools.iteritems():
-    print s
+for id, s in fbsSchools.iteritems():
+    print str(s) + ' (' + str(s.WinCount()) + ' - ' + str(s.LossCount()) + ')'
     
     for g in s.games:
         print g
